36eea9dea2d62124f1151427ac65b5e6409f024c
Alexander Gidzenko <gidzenko@mail.ru>
2024-08-28 21:50:02 +0300

    next commit

--- gendiff_python_project/scripts/gendiff.py
+++ gendiff_python_project/scripts/gendiff.py
@@ -4,11 +4,12 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument("fisrt_file")
     parser.add_argument("second_file")
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args)
+    print(args.format)
 
 
 if __name__ == '__main__':
